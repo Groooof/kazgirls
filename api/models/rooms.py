0729a9b413e2515816c7dcc98727c0966678f5a6
biktimirov <biktimirov@zvonok.com>
2025-10-25 21:09:30 +0300

    fix

--- api/models/rooms.py
+++ api/models/rooms.py
@@ -1,17 +1,7 @@
 from typing import TYPE_CHECKING
 
-from sqlalchemy import ForeignKey
-from sqlalchemy.orm import Mapped, mapped_column, relationship
-from sqlalchemy.sql.sqltypes import Integer, String
-
-from models.bases import (
-    BaseIdMixin,
-    BaseSQLAlchemyModel,
-    DateFieldsMixin,
-)
-
 if TYPE_CHECKING:
-    from models.user import User
+    pass
 
 
 {
@@ -31,32 +21,3 @@ if TYPE_CHECKING:
 class Streamer:
     user_id: int
     status: str  # online | offline
-
-
-class Room(BaseIdMixin[Integer], DateFieldsMixin, BaseSQLAlchemyModel):
-    __tablename__ = "rooms"
-    __engine__ = "default"
-
-    status: Mapped[str] = mapped_column(String(16))
-    streamer_id: Mapped[int] = mapped_column(ForeignKey("users.id"))
-    viewer_id: Mapped[int] = mapped_column(ForeignKey("users.id"), nullable=True)
-
-    streamer: Mapped["User"] = relationship()
-    viewer: Mapped["User"] = relationship()
-
-    def __str__(self) -> str:
-        return self.id
-
-
-class RoomMember(BaseIdMixin[Integer], DateFieldsMixin, BaseSQLAlchemyModel):
-    __tablename__ = "rooms_members"
-    __engine__ = "default"
-
-    room_id: Mapped[int] = mapped_column(ForeignKey("rooms.id"))
-    user_id: Mapped[int] = mapped_column(ForeignKey("users.id"))
-
-    room: Mapped["Room"] = relationship()
-    user: Mapped["User"] = relationship()
-
-    def __str__(self) -> str:
-        return f"room: {self.room_id} user: {self.user_id}"
