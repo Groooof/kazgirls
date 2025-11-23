9098650c460192d86965032a1f34007d91764744
Bulat <hufdgdgdgfd@mail.ru>
2025-11-24 01:42:50 +0300

    стрим

--- frontend/src/views/Streamer.vue
+++ frontend/src/views/Streamer.vue
@@ -6,7 +6,7 @@ import VideoPlayer from './VideoPlayer.vue'
 import axios from 'axios'
 import { config } from '@/config'
 
-const isProd = false
+const isProd = true
 
 const route = useRoute()
 const streamerId = isProd ? 4 : 2
