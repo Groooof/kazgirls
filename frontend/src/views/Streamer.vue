a37a2c70bb03acf23d35a7e969d1d506b33be614
Bulat <hufdgdgdgfd@mail.ru>
2025-12-02 23:38:46 +0300

    turn

--- frontend/src/views/Streamer.vue
+++ frontend/src/views/Streamer.vue
@@ -16,7 +16,7 @@ const rtcConfig: RTCConfiguration = {
       urls:  "stun:stun.l.google.com:19302"
     },
     {
-      urls: "turn:turn00.natpro.ru:3478",
+      urls: "turn:turn00.natpro.ru:5349",
       username: "test_09",
       credential: "test_09"
     }
